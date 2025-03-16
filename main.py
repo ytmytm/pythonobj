1d256b727ea75e8159a3505da370b0f4ecd9da96
Maciej Witkowiak <mwitkowiak@gmail.com>
2025-03-16 15:37:03 +0000

    argparse parameters

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
+import argparse
 
 from CsvExtractor import CsvExtractor
 from JsonLoader import JsonLoader
 from Deduplicator import Deduplicator
 from Job import Job
 
-job = Job.Job(
-    input_path = "pracownicy.csv",
-    output_path = "imiona.json",
-    extractor = CsvExtractor.CsvExtractor(),
-    transformer = Deduplicator.Deduplicator("Imię"),
-    loader = JsonLoader.JsonLoader(orient="records",index=False,lines=True)
-)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser("Job")
+    parser.add_argument('-i', '--input_path', type=str, help="CSV input",required=True)
+    parser.add_argument('-o', '--output_path', type=str, help="JSON output", default="output.json")
+    parser.add_argument('-f', '--fields', action='append', help="deduplication fields, e.g -f <field1> -f <field2> ", required=True)
+    parser.add_argument('--orient', type=str, default="records")
+    parser.add_argument('--index', type=bool, default=False)
+    parser.add_argument('--lines', type=bool, default=True)
+    args = parser.parse_args()
 
-job.run()
+    job = Job.Job(
+      input_path = args.input_path,
+      output_path = args.output_path,
+      extractor = CsvExtractor.CsvExtractor(),
+      transformer = Deduplicator.Deduplicator(args.fields),
+      loader = JsonLoader.JsonLoader(orient=args.orient,index=False,lines=True)
+    )
+
+    job.run()
